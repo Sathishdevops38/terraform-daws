11ae3ec40f6fa86b37149302c82a9ad775060e38
sathishdevops38 <sathishdevops38@gmail.com>
2025-10-14 09:54:48 +0530

    syntax

--- ec2.tf
+++ ec2.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "terraform" {
   ami = "var.ami_id"
   instance_type = "var.instance_type"
-  security_groups =  "var.sg_id" 
-  subnet_id = ["var.subnet_id"]
+  security_groups = ["var.sg_id"]
+  subnet_id = "var.subnet_id"
   tags = {
     Name = "terraform"
     Terraform = true
