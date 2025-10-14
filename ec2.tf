b841c91981bed65374375574494d4b300768a6f0
sathishdevops38 <sathishdevops38@gmail.com>
2025-10-14 10:25:01 +0530

    ami

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "terraform" {
-  ami = "ami-09c813fb71547fc4f"
+  ami = "ami-0b7ba67ca92bbe799"
   instance_type = "t3.medium"
   security_groups = [aws_security_group.allow-all.id]
   subnet_id = "subnet-06dc23984fd523dd0"
