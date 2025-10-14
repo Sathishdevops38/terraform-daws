64409c9304dceb96817fc78ce0debedb89582c60
sathishdevops38 <sathishdevops38@gmail.com>
2025-10-14 13:46:24 +0530

    ami update

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "terraform" {
-  ami = "ami-0b7ba67ca92bbe799"
+  ami = "ami-04c08fd8aa14af291"
   instance_type = "t3.medium"
   security_groups = [aws_security_group.allow-all.id]
   subnet_id = "subnet-06dc23984fd523dd0"
