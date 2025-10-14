eebcff167d98533a5a317573ce2a48279ac27c3a
sathishdevops38 <sathishdevops38@gmail.com>
2025-10-14 10:29:44 +0530

    name

--- ec2.tf
+++ ec2.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "terraform" {
   security_groups = [aws_security_group.allow-all.id]
   subnet_id = "subnet-06dc23984fd523dd0"
   tags = {
-    Name = "terraform"
+    Name = "2186585_terraform"
     Terraform = true
   }
 }
