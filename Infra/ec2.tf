803afbf5b1ff92dd21d87b3ac3992b1edaaefe70
sathishdevops38 <sathishdevops38@gmail.com>
2025-10-15 14:28:45 +0530

    enable public ip

--- Infra/ec2.tf
+++ Infra/ec2.tf
@@ -16,6 +16,7 @@ resource "aws_instance" "web_ec2" {
   ami = data.aws_ami.amazon_linux_2023.id
   instance_type = var.instance_type
   subnet_id = aws_subnet.robo_pub.id
+  associate_public_ip_address = true
   vpc_security_group_ids = [ aws_security_group.robo_sg_all.id ]
   tags = var.ec2_tags
   user_data = <<-EOF
